a9ca63e14c2b3159a99fec92ead32789b1f16656
olegreksha <masterjada@gmail.com>
2021-12-12 18:01:59 +0200

    Project files

---
+++ public/src/app.js
@@ -0,0 +1,63 @@
+
+const parent = document.querySelector("#map");
+const article = document.querySelector("#article");
+const title = document.querySelector("#name");
+const photosElement = document.querySelector("#photos");
+const backlight = document.querySelector("#backlight");
+const slider = document.querySelector("#image_slider");
+
+window.onload = ()=>{
+    displayPlaces(data);
+}
+
+function displayPlaces(data){
+    data.places.forEach(place => {
+        var element = document.createElement("img");
+        element.src = "src/" + place.type + ".png";
+        element.style.left = place.pos_x + "px";
+        element.style.top = place.pos_y + "px";
+        element.className = "site";
+        element.id = "elem_" + place.id;
+        
+        element.onclick = function(_ev){ 
+            var elem = document.getElementsByClassName("selected")[0];
+            if(elem){
+                elem.classList.remove("selected");
+            }
+            onElementClick(place)
+            element.classList.add("selected");
+        };
+        parent.appendChild(element);
+    });
+}
+
+function onElementClick(site){
+    console.log(site);
+    title.innerHTML = site.title;
+    article.innerHTML = site.describption;
+    fillPhotos(site.photos);
+}
+
+function fillPhotos(photos) {
+    photosElement.innerHTML = "";
+    backlight.style.backgroundImage = `url('src/${photos[0].src}')`;
+    photos.forEach( photo => {
+        var element = document.createElement("img");
+        element.src = "src/" + photo.src;
+        element.className = "photo_preview";
+        photosElement.appendChild(element);
+        element.onclick = () =>{
+            showSlider(photo);
+        }
+    });
+}
+
+function showSlider(photo){
+    slider.style.display = "flex";
+    const img = document.querySelector("#big_image");
+    img.src = "src/" + photo.src;
+}
+
+function closeSlider() {
+    slider.style.display = "none";
+}
---
+++ public/src/data.js
@@ -0,0 +1,80 @@
+let data ={
+    "name": "Романiв",
+    "places": [
+        {
+        "id": 0,
+        "type": "home_zoo",
+       "pos_x": 250,
+       "pos_y": 100,
+       "title": "Farm Zoo",
+       "describption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis justo eros. Mauris commodo libero turpis, ac fringilla lectus dignissim sit amet. Nunc risus elit, fringilla tempor velit in, fringilla ullamcorper ex. Nunc luctus aliquet ipsum, vitae suscipit tortor lobortis vitae. Duis nec mattis justo. Nullam id ullamcorper est. Nunc auctor lobortis ex non euismod. Aenean eget maximus leo. Curabitur sapien libero, laoreet vel sapien eu, aliquam pretium justo. Morbi accumsan nunc sed velit pharetra, non rhoncus neque tempus. Duis condimentum, nunc vel imperdiet ornare, magna enim pretium odio, et pulvinar dolor orci ut est. Sed id mollis libero.",
+       "photos": [
+            {
+                "name": "zoo",
+                "src": "zoo.jpeg"
+            }
+       ]
+    },
+    {
+        "id": 1,
+        "type": "pin",
+       "pos_x": 350,
+       "pos_y": 120,
+       "title": "Doll master",
+       "describption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis justo eros. Mauris commodo libero turpis, ac fringilla lectus dignissim sit amet. Nunc risus elit, fringilla tempor velit in, fringilla ullamcorper ex. Nunc luctus aliquet ipsum, vitae suscipit tortor lobortis vitae. Duis nec mattis justo. Nullam id ullamcorper est. Nunc auctor lobortis ex non euismod. Aenean eget maximus leo. Curabitur sapien libero, laoreet vel sapien eu, aliquam pretium justo. Morbi accumsan nunc sed velit pharetra, non rhoncus neque tempus. Duis condimentum, nunc vel imperdiet ornare, magna enim pretium odio, et pulvinar dolor orci ut est. Sed id mollis libero.",
+       "photos": [
+
+            {
+                "name": "mill",
+                "src": "doll.jpeg"
+            }
+       ]
+    },
+    {
+        "id": 2,
+        "type": "house",
+       "pos_x": 148,
+       "pos_y": 300,
+       "title": "Windmill",
+       "describption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis justo eros. Mauris commodo libero turpis, ac fringilla lectus dignissim sit amet. Nunc risus elit, fringilla tempor velit in, fringilla ullamcorper ex. Nunc luctus aliquet ipsum, vitae suscipit tortor lobortis vitae. Duis nec mattis justo. Nullam id ullamcorper est. Nunc auctor lobortis ex non euismod. Aenean eget maximus leo. Curabitur sapien libero, laoreet vel sapien eu, aliquam pretium justo. Morbi accumsan nunc sed velit pharetra, non rhoncus neque tempus. Duis condimentum, nunc vel imperdiet ornare, magna enim pretium odio, et pulvinar dolor orci ut est. Sed id mollis libero.",
+       "photos": [
+            {
+                "name": "mlyn",
+                "src": "mlyn.jpeg"
+            }
+       ]
+    },
+    {
+        "id": 3,
+        "type": "pin",
+       "pos_x": 300,
+       "pos_y": 30,
+       "title": "Dress",
+       "describption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis justo eros. Mauris commodo libero turpis, ac fringilla lectus dignissim sit amet. Nunc risus elit, fringilla tempor velit in, fringilla ullamcorper ex. Nunc luctus aliquet ipsum, vitae suscipit tortor lobortis vitae. Duis nec mattis justo. Nullam id ullamcorper est. Nunc auctor lobortis ex non euismod. Aenean eget maximus leo. Curabitur sapien libero, laoreet vel sapien eu, aliquam pretium justo. Morbi accumsan nunc sed velit pharetra, non rhoncus neque tempus. Duis condimentum, nunc vel imperdiet ornare, magna enim pretium odio, et pulvinar dolor orci ut est. Sed id mollis libero.",
+       "photos": [
+            {
+                "name": "doll",
+                "src": "girl.jpeg"
+            }
+       ]
+    },
+    {
+        "id": 4,
+        "type": "house",
+       "pos_x": 300,
+       "pos_y": 200,
+       "title": "Glay fabric",
+       "describption": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis justo eros. Mauris commodo libero turpis, ac fringilla lectus dignissim sit amet. Nunc risus elit, fringilla tempor velit in, fringilla ullamcorper ex. Nunc luctus aliquet ipsum, vitae suscipit tortor lobortis vitae. Duis nec mattis justo. Nullam id ullamcorper est. Nunc auctor lobortis ex non euismod. Aenean eget maximus leo. Curabitur sapien libero, laoreet vel sapien eu, aliquam pretium justo. Morbi accumsan nunc sed velit pharetra, non rhoncus neque tempus. Duis condimentum, nunc vel imperdiet ornare, magna enim pretium odio, et pulvinar dolor orci ut est. Sed id mollis libero.",
+       "photos": [
+            {
+                "name": "glay",
+                "src": "glay.jpeg"
+            },
+            {
+                "name": "doll",
+                "src": "doll.jpeg"
+            }
+       ]
+    },
+    ]
+}
